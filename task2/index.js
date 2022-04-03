f8d35471740bfe8e7a793a0ac5351b14fc96552c
Savkiv <ivan.savkiv.igorovych>
2022-04-03 07:16:54 +0300

    Fix task2 from Lesson1

--- task2/index.js
+++ task2/index.js
@@ -1,33 +1,6 @@
 function grid(N) {
   if (N < 0) return null;
-  let alphabet = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
   let bias = 0;
   let grid = "";
 
